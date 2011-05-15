1d4bdd8574bc03ed6955c84e34ba56f0bd4bf71a
Aleksei Kaminin <zendzirou@gmail.com>
2011-05-15 08:14:21 +0400

    Added text_inspector

--- lib/functions.php
+++ lib/functions.php
@@ -1,8 +1,8 @@
 <?php
 use bu\defun\Memo;
 defun('undefun', function($name){
-	      array_pop(Memo::$fns[$name]);
-	      end(Memo::$fns[$name]);
+		array_pop(Memo::$fns[$name]);
+		end(Memo::$fns[$name]);
 	});
 
 defun('funlet', function($fn){
@@ -33,42 +33,53 @@ defun('def_wrap', function($name, $fn){
 def_alias('undefun', 'undef_wrap');
 
 defun('def_printfer', function($name, $tpl){
-	defun($name, function() use($tpl){
-		$args = func_get_args();
-		array_unshift($args, $tpl);
-		return call_user_func_array('printf', $args);
+		defun($name, function() use($tpl){
+				$args = func_get_args();
+				array_unshift($args, $tpl);
+				return call_user_func_array('printf', $args);
+			});
 	});
-});
 
 defun('def_sprintfer', function($name, $tpl){
-	defun($name, function() use($tpl){
-		$args = func_get_args();
-		array_unshift($args, $tpl);
-		return call_user_func_array('sprintf', $args);
+		defun($name, function() use($tpl){
+				$args = func_get_args();
+				array_unshift($args, $tpl);
+				return call_user_func_array('sprintf', $args);
+			});
 	});
-});
 
 defun('def_memo', function($name, $fn){
-	defun($name, function() use($name, $fn){
-		static $data = array();
-		$args = func_get_args();
-		$key = serialize($args);
-		if(!isset($data[$name][$key]))
-			$data[$name][$key] = call_user_func_array($fn, $args);
-		return $data[$name][$key];
+		defun($name, function() use($name, $fn){
+				static $data = array();
+				$args = func_get_args();
+				$key = serialize($args);
+				if(!isset($data[$name][$key]))
+					$data[$name][$key] = call_user_func_array($fn, $args);
+				return $data[$name][$key];
+			});
 	});
-});
 
 defun('def_converter', function($from, $to, $fn){
-	defun("{$from}_to_{$to}", $fn);
-	defun("{$from}s_to_{$to}s", function($array) use ($fn){
-		return array_map($fn, $array);
+		defun("{$from}_to_{$to}", $fn);
+		defun("{$from}s_to_{$to}s", function($array) use ($fn){
+				return array_map($fn, $array);
+			});
 	});
-});
 
 // Объявить функцию $name, которая просто вернёт $value.
 defun('def_ret', function($name, $value){
 		defun($name, function() use($value){
 				return $value;
+			});
+	});
+
+defun('def_text_inspector', function($fn){
+		def_wrap($fn, function($call) use($fn){
+				echo ">> Calling '{$fn}' function with arguments:\n";
+				foreach($call->args as $arg)
+					echo ">> - {$arg}\n";
+				$r = $call();
+				echo ">> return value is '{$r}'\n";
+				return $r;
 			});
 	});
---
+++ tests/021.php
@@ -0,0 +1,17 @@
+<?php
+set_time_limit(1);
+defun('inc', function($i){
+		echo "Increment {$i}\n";
+		return $i + 1;
+	});
+
+def_text_inspector('inc');
+echo inc(3, 'hello');
+?>
+---
+>> Calling 'inc' function with arguments:
+>> - 3
+>> - hello
+Increment 3
+>> return value is '4'
+4
