d7f26a8053153e2bf8456a32aa8f73a206ae5c3a
Aleksei Kaminin <zendzirou@gmail.com>
2011-05-22 23:18:22 +0400

    Tests moved to separated dir

--- run-test.php
+++ run-test.php
@@ -3,42 +3,54 @@
 require_once 'load.php';
 def_printfer("puts","%s\n");
 
-def('test_files', function(){
-	return glob('tests/*.php');
-});
+def('test_dirs', function(){
+		return glob('tests/*/');
+	});
 
 def('read_test', function($file){
-	return explode("---\n", file_get_contents($file));
-});
+		return explode("---\n", file_get_contents($file));
+	});
 
 def('eval_output', function($src){
-	file_put_contents('tmp.php', $src);
-	return `php tmp.php`;
-});
+		file_put_contents('tmp.php', $src);
+		return `php tmp.php`;
+	});
 
 def('add_error', function($file, $eval_out, $result){
-	puts("Error in file: {$file}");
-	puts($eval_out);
-	puts('------------------');
-	puts($result);
-	puts('');
-});
+		puts("Error in file: {$file}");
+		puts($eval_out);
+		puts('------------------');
+		puts($result);
+		puts('');
+	});
+
+def('test_files', function($dir){
+		return glob($dir.'*.php');
+	});
 
 $correct = 0;
 $total = 0;
 $fail = 0;
-foreach(test_files() as $file){
-	$total++;
-	list($src, $result) = read_test($file);
-	$result = trim($result); 
-	$eval_out = trim(eval_output("<? require_once 'load.php'; ?>\n".$src));
-	if($eval_out == $result){
-		$correct++;
-		echo '.';
-	}else{
-		add_error($file, $eval_out, $result);
-		$fail++;
-		echo '!';
+foreach(test_dirs() as $dir){
+	$prefix = ""; $suffix = "";
+	if(file_exists($dir.'prefix'))
+		$prefix = file_get_contents($dir.'prefix');
+	if(file_exists($dir.'suffix'))
+		$suffix = file_get_contents($dir.'suffix');
+	foreach(test_files($dir) as $file){
+		$total++;
+		list($src, $result) = read_test($file);
+		$result = trim($result);
+
+		$eval_out = trim(eval_output($prefix.$src.$suffix));
+		if($eval_out == $result){
+			$correct++;
+			echo '.';
+		}else{
+			add_error($file, $eval_out, $result);
+			$fail++;
+			echo '!';
+		}
 	}
 }
 puts('');
