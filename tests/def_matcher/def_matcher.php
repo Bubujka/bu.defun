b1324ddc3745701ac4fd39ceb1d087b8d5283cc2
Aleksei Kaminin <zendzirou@gmail.com>
2011-08-03 21:26:51 +0400

    Added doc-string to def_matcher

--- tests/def_matcher/def_matcher.php
+++ tests/def_matcher/def_matcher.php
@@ -1,6 +1,10 @@
 <?php
+// def_matcher - объявляет функцию, которая сопоставляет свой аргумент с
+// регулярным выражением
 echo (int)function_exists('is_az');
+
 def_matcher('is_az', '/^[a-z]+$/');
+
 echo (int)function_exists('is_az');
 foreach(array('', '1', 'a2', 'E', '_') as $v)
 	echo (int)is_az($v);
