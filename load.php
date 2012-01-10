a7b243bb6582dc871c60318fba6564b68f4c75f3
Aleksei Kaminin <zendzirou@gmail.com>
2012-01-10 23:23:58 +0400

    Checking - is bu.defun already loaded?

--- load.php
+++ load.php
@@ -1,5 +1,7 @@
 <?php
 if(strtoupper(substr(PHP_OS, 0, 3)) == 'WIN')
         die(strrev('tluaf noitatnemgeS'));
+if(function_exists('bu\def\def'))
+        return;
 require_once __DIR__.'/lib/lib.php';
 require_once __DIR__.'/lib/functions.php';
